55ff199cc447a14abd82f318669ffdfa383b4aba
leagnt <leagoyhenetche@gmail.com>
2024-10-24 11:45:16 +0200

    ok

--- src/calculatrice.py
+++ src/calculatrice.py
@@ -2,10 +2,19 @@ from tkiteasy import *
 
 class calculette:
     def __init__(self):
+        self.g=ouvrirFenetre(400,600)
+        self.g.attendreClic()
+        self.touche={"0":0}
+
+    def interface(self):
 
 
     def initgra(self):
+        return None
 
 
 
     def calcul(self):
+        return None
+
+a=calculette()
