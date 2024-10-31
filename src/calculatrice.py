0449f6c4040ef271008d304350002b0fbe41d2d8
Felix55558877 <felix.bvr4@gmail.com>
2024-10-31 13:19:06 +0100

    test

--- src/calculatrice.py
+++ src/calculatrice.py
@@ -5,6 +5,7 @@ class calculette:
         self.g=ouvrirFenetre(400,600)
         self.touche={"0":(133,520),"=":(266,520),"1":(80,440),"2":(160,440),"3":(240,440),"+":(320,440),"4":(80,360),"5":(160,360),"6":(240,360),"-":(320,360),"7":(80,280),"8":(160,280),"9":(240,280),"x":(320,280),"AC":(100,200),"DEL":(200,200),"/":(300,200)}
         self.interface()
+        self.entree = ""
     def interface(self):
         self.g.dessinerRectangle(0,0,400,600,"pink")
         self.g.dessinerRectangle(30,30,340,120,"grey")
@@ -13,10 +14,10 @@ class calculette:
             self.g.dessinerDisque(x,y,30,"blue")
             self.g.afficherTexte(touche,x,y,"white",18)
         self.g.actualiser()
-
-
-    def fonc(self):
-        self.recupererchiffre()
+    def resultat(self, resultat):
+        self.g.dessinerRectangle(30, 30, 340, 120, "grey")  # Efface l'écran
+        self.g.afficherTexte(str(resultat), 200, 70, "black", 24)
+        self.g.actualiser()
 
     def recupererchiffre(self):
         recup=False
@@ -31,89 +32,60 @@ class calculette:
                         choix = touche
         return choix
 
-    def initgra(self):
-        return None
-    def initchiffre(self):
-        chiffres = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        entree = ""
-        recup = False
-        while recup == False:
-            nombre = self.recupererchiffre()
-            if str(nombre) in chiffres:
-                entree += nombre
-
-    def creechiffre(self):
-        chiffres = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        entree = ""
-        recup = False
-        while recup == False:
-            nombre = self.recupererchiffre()
-            if str(nombre) in chiffres:
-                entree += nombre
-            if nombre == "+":
-                return (int(entree), "+")
-            if nombre == "-":
-                return (int(entree), "-")
-            if nombre == "x":
-                return (int(entree), "x")
-            if nombre == "AC":
-                return (int(entree), "AC")
-            if nombre == "DEL":
-                return (int(entree), "DEL")
-            if nombre == "/":
-                return (int(entree), "/")
     def initcalcul(self):
-        chiffres = ["0","1","2","3","4","5","6","7","8","9"]
-        entree = ""
+        chiffres = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        self.entree = ""
+        operation = None
+        resultat = 0
         recup = False
+
         while recup == False:
             nombre = self.recupererchiffre()
             if str(nombre) in chiffres:
-                entree += nombre
-            if nombre == "+":
-                return self.calcul(int(entree),"+")
-            if nombre == "-":
-                return self.calcul(int(entree), "-")
-            if nombre == "x":
-                return self.calcul(int(entree),"x")
-            if nombre == "AC":
-                return self.calcul(int(entree),"AC")
-            if nombre == "DEL":
-                entree = pop.entree
-            if nombre == "/":
-                return self.calcul(int(entree),"/")
+                self.entree += nombre
+                self.resultat(self.entree)
+
+            elif nombre == "+" or nombre == "-" or nombre == "x" or nombre == "/":
+                if self.entree is not None:
+                    resultat = int(self.entree)
+                    operation = nombre
+                    self.entree += operation  # Ajoute l'opération à l'affichage
+                    self.resultat(self.entree)  # Affiche l'opération en cours
+                    self.entree = ""
+
+            elif nombre == "=":
+                if operation is not None and self.entree is not None:
+                    resultat = self.calcul(int(resultat), int(self.entree), operation)
+                    self.resultat(resultat)
+                    self.entree = str(resultat)
+                    operation = None
 
 
+            elif nombre == "AC":
+                self.entree = ""
+                resultat = 0
+                operation = None
+                self.resultat(0)
 
+            elif nombre == "DEL":
+                self.entree = self.entree[:-1]
+                self.resultat(self.entree if self.entree is not None else 0)
 
-
-    def calcul(self, nombre, operation):
+    def calcul(self, nombre1, nombre2, operation):
         if operation == "+":
-            nv_nombre, operation2 = self.creechiffre()
-            resultat = nombre + int(nv_nombre)
-            return self.calcul(resultat,operation2)
+            return nombre1 + nombre2
 
         if operation == "-":
-            nv_nombre, operation2 = self.creechiffre()
-            resultat = nombre - int(nv_nombre)
-            return self.calcul(resultat, operation2)
+            return nombre1 - nombre2
 
         if operation == "x":
-            nv_nombre, operation2 = self.creechiffre()
-            resultat = nombre * int(nv_nombre)
-            return self.calcul(resultat, operation2)
+            return nombre1*nombre2
 
-        if operation == "division":
-            nv_nombre, operation2 = self.creechiffre()
-            resultat = nombre / int(nv_nombre)
-            return self.calcul(resultat, operation2)
-
-        if operation == "DEL":
-            nv_nombre = str(nombre)
-            nv_nombre1 = pop.nv_nombre
-            return self.calcul()
-        if operation == "resultat":
-            return nombre
+        if operation == "/":
+            if nombre2 != 0:
+                return nombre1 / nombre2
+            else:
+                return "Erreur"
 
 
 
@@ -121,5 +93,5 @@ class calculette:
 
 
 a=calculette()
-a.fonc()
+a.initcalcul()
 
